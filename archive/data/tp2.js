bd1dfef68f5c06673f7708f918ed4140406c91aa
kim841 <kim.trinh@umontreal.ca>
2023-11-18 18:19:18 -0500

    render planets, orbits and lagrange points

--- archive/data/tp2.js
+++ archive/data/tp2.js
@@ -11,6 +11,30 @@ let ambient_light;
 let camera_light;
 let last_render = Date.now();
 
+const color = {
+    rouge: 0xff0000,
+    vert: 0x00ff00,
+    bleu: 0x0000ff,
+    jaune: 0xffff00,
+    cyan: 0x00ffff,
+    orange: 0xffa500,
+    blanc: 0xffffff,
+    vertFonce: 0x006400,
+}
+let L1, L2, L3, L4, L5; //Points de lagrange
+let orbitSatteliteJamesWebb;
+
+const planetRadius = {
+    earth: 0.05,
+    sun: 0.09,
+}
+
+const orbitRadius = {
+    earthAroundSun: 1,
+    othersAroundSun: 2,
+}
+
+
 // NOTE: vous pouvez ajouter des variables globales au besoin.
 
 
@@ -23,20 +47,43 @@ function createScene() {
     // TODO: Dessiner les étoiles
 
     // TODO: Dessiner le soleil
-    draw_sun(0.09);
-    // TODO: Dessiner l'orbite de la planète
-    scene.add(draw_orbit(0.2));
+    draw_sun(planetRadius.sun);
+
+    // // TODO: Dessiner l'orbite de la planète
+    scene.add(draw_orbit(orbitRadius.earthAroundSun, color.blanc));
     
     // Dessiner le système Terre-Lagrange
     // TODO: dessiner la planète
-    scene.add(draw_earth(0.05));
+    scene.add(draw_earth(planetRadius.earth));
+    L2 = draw_laGrange(color.vert, 0, {x:1.5, y:0, z:0});
+    L2 = draw_laGrange(color.vert, 1.3, {x:1.5, y:0, z:0});
+    scene.add(L2);
+
+    orbitSatteliteJamesWebb = draw_orbit((1/8.0)*orbitRadius.earthAroundSun, color.vertFonce);
+    orbitSatteliteJamesWebb.position.set(1,0,0);
+    scene.add(orbitSatteliteJamesWebb);
 
     // TODO: Ajout d'une source de lumière directionnelle
+    var light = new THREE.DirectionalLight(0xFFFFFF)
+    light.position.set(1, 0, 0)
+    // const helper = new THREE.DirectionalLightHelper( light, 5 );
+    // scene.add( helper );
+    scene.add(light)
 
     // TODO: Dessiner les points de Lagrange et l'orbite L2
-    scene.add(draw_orbit(2));
-    generate_pyramid_IFS();
-    // scene.add(draw_pyramid())
+    scene.add(draw_orbit(orbitRadius.othersAroundSun, color.blanc));
+
+    L1 = draw_laGrange(color.rouge, 0.7, {x:0.5, y:0, z:0});
+    scene.add(L1);
+
+    L3 = draw_laGrange(color.bleu, 1, {x: -1, y:0, z:0});
+    scene.add(L3);
+
+    L4 = draw_laGrange(color.jaune, 0.7, {x: 0.75, y:1, z:0});
+    scene.add(L4);
+
+    L5 = draw_laGrange(color.cyan, 0.7, {x:0.75, y:-1, z:0});
+    scene.add(L5);
 
     // Créer une caméra
     camera = new THREE.PerspectiveCamera(45, canvas.width/canvas.height, 0.1, 100);
@@ -60,12 +107,7 @@ function generate_randomStars() {
 
 function generate_pyramid_IFS(){
     let model = {}
-    // let vertices = {
-    //     v1: [Math.SQRT(8/parseFloat(9)),0,-1/parseFloat(3)], 
-    //     v2: [- Math.sqrt(2/parseFloat(9)),Math.sqrt(2/parseFloat(3)),-1/parseFloat(3)], 
-    //     v3: [- Math.sqrt(2/parseFloat(9)),-Math.sqrt(2/parseFloat(3)),-1/parseFloat(3)], 
-    //     v4: [0,0,1]
-    // }
+
     let vertices = new Float32Array([
         Math.sqrt(8/parseFloat(9)),0,-1/parseFloat(3),  
         - Math.sqrt(2/parseFloat(9)),Math.sqrt(2/parseFloat(3)),-1/parseFloat(3),
@@ -76,47 +118,41 @@ function generate_pyramid_IFS(){
         0,3,2,  1,3,0,  2,3,1,  0,2,1
     ];
 
-    const geometry = new THREE.BufferGeometry();
-    geometry.setIndex( indicesOfFaces );
-    geometry.setAttribute( 'position', new THREE.BufferAttribute( vertices, 3 ) );
-    const material = new THREE.MeshBasicMaterial( { color: 0xff0000 } );
-    const mesh = new THREE.Mesh( geometry, material );
-
-    geometry.computeVertexNormals();
-    const helper = new VertexNormalsHelper( mesh, 1, 0xff0000 );
-
-    scene.add( mesh );
-    scene.add( helper );
-
     model = {vertices, indicesOfFaces}
    
-    // let faces = {f1:[vertices.v1,vertices.v4,vertices.v3],
-    //             f2:[vertices.v2,vertices.v4,vertices.v1],
-    //             f3:[vertices.v3,vertices.v4,vertices.v2],
-    //             f4:[vertices.v1,vertices.v3,vertices.v2]} //under
-    
-    // TODO: créer le modèle IFS de la pyramide
     return model
 }
 
-function draw_pyramid() {
+function draw_pyramid(color) {
     let pyramid = null;
     // TODO: dessiner la pyramide
     let pyramidIFS = generate_pyramid_IFS();
-    const geometry = new THREE.PolyhedronGeometry( pyramidIFS.vertices, pyramidIFS.indicesOfFaces, 0.1, 1 );
-    // const geometry = new THREE.BufferGeometry( pyramidIFS.vertices, pyramidIFS.indicesOfFaces, 0.1, 1 );
-
-    // const geometry = new THREE.TetrahedronGeometry( pyramidIFS.vertices, pyramidIFS.indicesOfFaces, 1, 1 );
-    const material = new THREE.MeshBasicMaterial( { color: 0xff0000 } );
+    const geometry = new THREE.BufferGeometry();
+    geometry.setIndex( pyramidIFS.indicesOfFaces );
+    geometry.setAttribute( 'position', new THREE.BufferAttribute( pyramidIFS.vertices, 3 ) );
+    const material = new THREE.MeshBasicMaterial( { color: color } );
     pyramid = new THREE.Mesh( geometry, material );
-    pyramid.position.x = 1;
+
+    geometry.computeVertexNormals();
+    const helper = new VertexNormalsHelper( pyramid, 1, 0xff0000 );
+
+    // scene.add( mesh );
+    // scene.add( helper );
     return pyramid
+}
+
+function draw_laGrange(color, scale, position) {
+    let laGrange = draw_pyramid(color);
+    laGrange.scale.set(scale,scale,scale);
+    laGrange.position.set(position.x, position.y, position.z);
+
+    return laGrange
 }
 
 function draw_sun(radius) {
     // TODO: dessiner le soleil
     const geometry = new THREE.SphereGeometry( radius, 32, 16 ); 
-    const material = new THREE.MeshBasicMaterial( { color: 0xffff00 } ); 
+    const material = new THREE.MeshBasicMaterial( { color: color.orange } ); 
     const sphere = new THREE.Mesh( geometry, material );
     scene.add( sphere );
 
@@ -126,9 +162,9 @@ function draw_earth(radius) {
     let earth = null;
     // TODO: dessiner la planète
     const geometry = new THREE.SphereGeometry( radius, 32, 16 ); 
-    const material = new THREE.MeshBasicMaterial( { color: 0x0000ff } ); 
+    const material = new THREE.MeshLambertMaterial( { color: color.blanc } ); 
     earth = new THREE.Mesh( geometry, material );
-    earth.position.x = 0.2;
+    earth.position.x = 1;
     
     return earth
 }
@@ -137,11 +173,11 @@ function draw_stars() {
     // TODO: dessiner les étoiles
 }
 
-function draw_orbit(radius){
+function draw_orbit(radius, color){
     let orbit = null;
     // TODO: dessiner l'orbite de la planète
     const geometry = new THREE.RingGeometry( radius, radius + 0.01, 100 ); 
-    const material = new THREE.MeshBasicMaterial( { color: 0xffff00, side: THREE.DoubleSide } );
+    const material = new THREE.MeshBasicMaterial( { color: color, side: THREE.DoubleSide } );
     orbit = new THREE.Mesh( geometry, material ); 
     return orbit
 }
@@ -171,11 +207,16 @@ function animate() {
     let run_animation = document.getElementById("toggleAnimation");
     if (run_animation.checked) {
         // TODO: animer la scène
+
     }   
     last_render = Date.now();
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
     
+}
+
+function degresToRadians(angle) {
+    return angle * (Math.PI / 180.0);
 }
 
 function init() {
@@ -195,6 +236,7 @@ function init() {
     //pyramidIFS = generate_pyramid_IFS(); // TODO (a): Décommenter cette ligne
     
     // TODO (a): calcul des positions des points de Lagrange
+
 
     // TODO: Importation des textures
 
